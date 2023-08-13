0600ec29a37bebd8f21c60ea93ac9d6562e7c2f1
minhook <140972000+minhooks@users.noreply.github.com>
2023-08-13 15:07:43 -0300

    Initial commit

---
+++ dc-streaming-status/Program.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Gateway;
+
+using static System.Reflection.Assembly;
+using dc_streaming_status.Source;
+
+namespace dc_streaming_status
+{
+    class Program
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        /* If you prefer not to input the token every time the program runs, you can  set it directly here as a string. */
+        private static string _token = "YOUR_TOKEN_HERE";
+
+        static void Main()
+        {
+            Console.WriteLine("");
+
+            Logger.Info( $"streaming status v{ GetExecutingAssembly().GetName().Version }\n" );
+
+            if ( _token == "YOUR_TOKEN_HERE" ) {
+                _token = Logger.Log( "enter your token: " );
+
+                Console.Clear();
+
+                Console.WriteLine("");
+                Logger.Info($"streaming status v{GetExecutingAssembly().GetName().Version}\n");
+            }
+
+            // messy code ik lol 
+
+            Logger.Info($"trying to log in.\n");
+
+            DiscordSocketClient client = new DiscordSocketClient();
+
+            try
+            {
+                client.Login(_token);
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn($"an error occured: {ex.Message}");
+            }
+
+            Logger.Okay($"logged in as {client.User.Username}.\n");
+
+            string streamText = Logger.Log("enter the stream name: ");
+            string streamUrl = Logger.Log("enter the stream url: ");
+
+            client.UpdatePresence(new PresenceProperties()
+            {
+                Status = UserStatus.DoNotDisturb,
+                Activity = new StreamActivityProperties() { Name = streamText, Url = streamUrl }
+            });
+
+            if (Logger.Log("do you want to hide the console window and run in the background? (y/n): ").ToLower() == "y") ShowWindow(GetConsoleWindow(), 0);
+
+            Thread.Sleep(-1);
+        }
+
+    }
+}
---
+++ dc-streaming-status/Source/Logger.cs
@@ -0,0 +1,16 @@
+﻿
+namespace dc_streaming_status.Source
+{
+    internal class Logger
+    {
+        public static void Info(string msg) => Console.WriteLine($" [i] {msg}");
+        public static void Warn(string msg) => Console.WriteLine($" [-] {msg}");
+        public static void Okay(string msg) => Console.WriteLine($" [+] {msg}");
+        
+        public static string Log(string msg)
+        {
+            Console.Write($" [?] {msg}");
+            return Console.ReadLine();
+        }
+    }
+}
